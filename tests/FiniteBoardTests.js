43c1d58901ab3453be46a2beb0c14bd5b7a89d0d
Yelena Gindin <yelenar@gmail.com>
2017-01-15 09:24:50 +0200

    one more test

--- tests/FiniteBoardTests.js
+++ tests/FiniteBoardTests.js
@@ -1,6 +1,19 @@
 var should = require('should');
 var FiniteBoard = require('../src/FiniteBoard.js')
 
+function nLive(board) {
+    var n = 0;
+
+    for (var x = 0; x < board.width(); x++) {
+        for (var y = 0; y < board.height(); y++) {
+            if (board.isLive(x, y)) {
+                n++;
+            }
+        }
+    }
+
+    return n;
+}
 
 describe('Finite life test', function() {
     var board;
@@ -16,6 +29,23 @@ describe('Finite life test', function() {
     it('create', function() {
         board.width().should.be.equal(20);
         board.height().should.be.equal(10);
+    });
+
+    it('single cell death', function() {
+        board.setLive(5, 5);
+        nLive(board).should.be.equal(1);
+        board.step();
+        nLive(board).should.be.equal(0);
+    });
+
+    it('square live forever', function() {
+        board.setLive(5, 5);
+        board.setLive(5, 6);
+        board.setLive(6, 5);
+        board.setLive(6, 6);
+        nLive(board).should.be.equal(4);
+        board.step();
+        nLive(board).should.be.equal(4);
     });
 
 });
